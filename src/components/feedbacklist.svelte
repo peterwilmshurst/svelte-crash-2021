8a6b0158cab317151c63326cdac7775afe4a7924
Peter Wilmshurst <wilmshurstp@rsc.org>
2022-08-30 16:47:43 +0100

    add svelte transitions

--- src/components/feedbacklist.svelte
+++ src/components/feedbacklist.svelte
@@ -1,9 +1,12 @@
 <script>
+import {fade, scale} from 'svelte/transition';
 import FeedbackItem from "./feedbackitem.svelte";
 export let feedback = []
 console.log(feedback)
 </script>
 
 {#each feedback as fb (fb.id)}
+<div in:scale out:fade="{{ duration: 500 }}">
 <FeedbackItem item={fb} on:delete-feedback />
+</div>
 {/each}
